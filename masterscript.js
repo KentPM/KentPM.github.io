5da9dc55cb812f931d7e7288c526275cbba22df3
KentPM <kent.pm@gmail.com>
2015-02-04 01:19:53 -0800

    2/4-1

--- masterscript.js
+++ masterscript.js
@@ -1,13 +1,24 @@
-$(document).ready(){};
-$('#bClick').onclick(function()){
-alert("you have clicked this button");
-}
-function timeServed()={
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+
+$( "#bClick" ).click(function() {
+  alert( "Handler for .click() called." );
+});
+
+
+
+
+});
+
+
+
+    
+
+
+
+function timeServed(){
     var startTime= document.getElementById('hireDate').value;
     var today = new Date();
     var timeS = today - startTime;
-    alert(timeS);
-    
-}
-
-timeServed();
+    alert(timeS);}
