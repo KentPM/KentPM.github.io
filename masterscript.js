b9bcbbcfbb21e9f7dbc65034573325707f4c97bc
KentPM <kent.pm@gmail.com>
2015-02-05 03:16:32 -0800

    2/5
    Large changes - functioning wage calculator implemented.

--- masterscript.js
+++ masterscript.js
@@ -1,10 +1,7 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
+$(document).ready(function () {
+    welcome();
+    alert("These Calculation Are Rounded, Variances may occur")
 
-
-$( "#bClick" ).click(function() {
-  alert( "Handler for .click() called." );
-});
 
 
 
@@ -12,8 +9,56 @@ $( "#bClick" ).click(function() {
 });
 
 
+function welcome() {
+    $('#fun').click(function () {
+        $('td').show('swing');
+        st = '$';
+        
+        
+        var wage = $('#stuff').val();
+        var ste = [2085.6, 2294.16, 2555, 3893.44];
 
-    
+        var multi = [1, 1.5, 0.5];
+        var rates8 = [];
+        var rates10 = [];
+        var rates12 = [];
+        var rates24 = [];
+        var totals = [rates8, rates10, rates12, rates24];
+        for (i = 0; i < 3; i++) {
+            rates8[i] = rounder(wage, ste[0], multi[i]);
+            rates10[i] = rounder(wage, ste[1], multi[i]);
+            rates12[i] = rounder(wage, ste[2], multi[i]);
+            rates24[i] = rounder(wage, ste[3], multi[i]);
+        }
+        rates8[3] = rounder(rates8[0], 1, 1.5)
+        rates10[3] = rounder(rates10[0], 1, 1.65)
+        rates12[3] = rounder(rates12[0], 1, 1.75)
+        rates24[3] = rounder(rates24[0], 1, 2)
+
+
+
+        var mod = ['r', 'ot', 'ho', 'p']
+        for (t = 0; t < 4; t++) {
+            $('#hr8' + mod[t]).text(st + totals[0][t]);
+        }
+
+        for (t = 0; t < 4; t++) {
+            $('#hr10' + mod[t]).text(st + totals[1][t]);
+        }
+
+        for (t = 0; t < 4; t++) {
+            $('#hr12' + mod[t]).text(st + totals[2][t]);
+        }
+        for (t = 0; t < 4; t++) {
+            $('#hr24' + mod[t]).text(st + totals[3][t]);
+        }
+
+        function rounder(w, s, m) {
+            return Math.floor((w / s * m) * 100) / 100;
+        }
+
+    });
+}
 
 
 
